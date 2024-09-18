f5d2780c1ec8717d78e741df801ad9ce3d60ec1d
thequlmatov <zuxroka0104@gmail,com>
2024-09-18 21:13:12 +0500

    route-configuration

--- src/view/Generics/index.jsx
+++ src/view/Generics/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import { Container } from "./style";
 
 const Generic = () => {
+  const location = useLocation();
   return (
     <Container>
-      <h1>{location.pathname}</h1>
+      <h1>{location.pathname} page</h1>
       <h1>Coming soon ...</h1>
     </Container>
   );
