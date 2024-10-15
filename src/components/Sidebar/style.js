e8c90689a384e3d74011798322002ee1ef3091c8
thequlmatov <zuxroka0104@gmail,com>
2024-10-15 23:37:29 +0500

    sidebar setting

--- src/components/Sidebar/style.js
+++ src/components/Sidebar/style.js
@@ -9,7 +9,7 @@ const Side = styled.div`
   max-width: 280px;
   min-width: 280px;
   background-color: white;
-  border: 2px solid red;
+  /* border: 2px solid red; */
   overflow: hidden;
   overflow-y: scroll;
   &::-webkit-scrollbar {
@@ -25,5 +25,19 @@ const Wrapper = styled.div`
   margin: 16px;
   background-color: white;
 `;
-
-export { Container, Side, Body, Wrapper };
+const Logo = styled.div`
+  font-weight: 600;
+  font-size: 20px;
+  font-style: normal;
+  line-height: 28px;
+  color: #1890ff;
+  padding: 16px 24px;
+  border-bottom: 1px solid rgba(222, 225, 227, 1);
+  cursor: pointer;
+`;
+const LogOut = styled(Logo)`
+  border-top: 1px solid rgba(222, 225, 227, 1);
+  border-bottom: 0;
+  position: relative;
+`;
+export { Container, Side, Body, Wrapper, Logo, LogOut };
